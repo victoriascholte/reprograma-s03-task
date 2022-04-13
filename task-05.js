971ff0a7056e0644869f23659cdb597001fa1232
Victoria Scholte <victoria.scholte@gmail.com>
2022-04-12 21:54:11 -0300

    fixes in task-03 and task-05

--- task-05.js
+++ task-05.js
@@ -1,14 +1,14 @@
 // 5 - Crie uma função que recebe o ano de nascimento 
 // da pessoa informando se ela é maior de idade ou menor.
 
-function verifyAge(age) {
-    if (age >= 18){
+function verifyAge(year) {
+    if (year < 2002){
         return "Essa pessoa é maior de idade."
     }
-    else if (age < 18) {
+    else if (year >= 2004) {
         return "Essa pessoa é menor de idade."
     }
 }
 
-console.log(verifyAge(23))
-console.log(verifyAge(4))
+console.log(verifyAge(1923))
+console.log(verifyAge(2004))
